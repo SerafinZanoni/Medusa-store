4fcfc8e702151dfea4c26e73a47e5d0c6ca1b531
Serafin Zanoni <serazanoni@gmail.com>
2025-02-27 16:51:12 -0300

    first commit

---
+++ medusa-store-storefront/src/modules/home/components/hero/index.tsx
@@ -0,0 +1,36 @@
+import { Github } from "@medusajs/icons"
+import { Button, Heading } from "@medusajs/ui"
+import LocalizedClientLink from "@modules/common/components/localized-client-link"
+import Link from "next/link"
+
+const Hero = () => {
+  return (
+    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
+        <span>
+          <Heading
+            level="h1"
+            className="text-3xl leading-10 text-ui-fg-base font-normal"
+          >
+            Ecommerce made easy
+          </Heading>
+          <Heading
+            level="h2"
+            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
+          >
+            Powered by Medusa and Next.js
+          </Heading>
+        </span>
+        <div className="mt-4">
+          <Link href="/store">
+            <Button data-testid="continue-shopping-button">
+              Continue shopping
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
